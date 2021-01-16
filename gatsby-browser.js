c1ff9690c0886ad059d4da06a47dd662957db45a
Isaac <>
2021-01-16 15:56:52 -0500

    added cache busting

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,22 @@
 import "./src/resources/css/global.css"
+import React from "react"
+import { Provider } from 'react-redux';
+import CacheBuster from "./CacheBuster.js"
+
+import { createStore, applyMiddleware } from 'redux';
+
+const store = createStore(() => [], {}, applyMiddleware());
+
+export const wrapRootElement = ({ element }) => (
+     <CacheBuster>
+        {({ loading, isLatestVersion, refreshCacheAndReload }) => {
+          if (loading) return null
+          if (!loading && !isLatestVersion) {
+            // You can decide how and when you want to force reload
+            refreshCacheAndReload()
+          }
+          console.log(element)
+          return <Provider store={store}>{element}</Provider>
+        }}
+      </CacheBuster>
+    )
